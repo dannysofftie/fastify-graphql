e4fdeabfdf1a3760e7ceb36fda4212aad1d4417b
dannysofftie <dankim761@gmail.com>
2021-03-19 12:35:14 +0300

    Prisma schema and migrations

---
+++ prisma/migrations/20210319043649_optional_email/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "accounts" ALTER COLUMN "idNumber" SET DATA TYPE TEXT;
---
+++ prisma/migrations/20210319073445_change_password_type/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "accounts" ALTER COLUMN "password" SET DATA TYPE VARCHAR(255);
