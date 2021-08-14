5256e9f6643b96d7dcc2d846b67b957f0f166b3e
dannysofftie <dankim761@gmail.com>
2021-08-14 18:23:41 +0300

    Remove stale migrations

--- prisma/migrations/20210319042811_optional_email/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `phone` on table `accounts` required. The migration will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "accounts" ALTER COLUMN "email" DROP NOT NULL,
-ALTER COLUMN "phone" SET NOT NULL;
--- prisma/migrations/20210319073445_change_password_type/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "accounts" ALTER COLUMN "password" SET DATA TYPE VARCHAR(255);
