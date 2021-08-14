a01dd12a596706950bcafdba3f3672a6c756b6cb
dannysofftie <dankim761@gmail.com>
2021-08-14 17:01:39 +0300

    Update typings

--- index.d.ts
+++ index.d.ts
@@ -1,20 +1,46 @@
-import { Browser } from 'puppeteer';
 import { IConfig } from '@configs';
-import { IFirebase } from '@libs/Firebase';
+import { ILibraries } from '@libs/index';
 import { IPlugins } from '@plugins/index';
 import { IJWTPayload } from '@plugins/Token';
-import { PrismaClient } from '@prisma/client';
+import { ReadStream } from 'fs';
+
+declare global {
+  interface IGCPUpload {
+    localStream: ReadStream;
+    contentType: string;
+    remotePath: string;
+  }
+  /**
+   * Incoming GraphQL file stream.
+   *
+   * This is as provided by `Upload` scalar
+   */
+  interface IUploadFileStream {
+    createReadStream(): ReadStream;
+    mimetype: string;
+    filename: string;
+    encoding: string;
+  }
+
+  /**
+   * Attachment properties. Every uploaded file must have all the properties
+   */
+  interface IUploadAttachment {
+    file: string;
+    mime: string;
+    filename: string;
+    encoding: string;
+  }
+}
 
 declare module 'fastify' {
-    interface FastifyInstance {
-        configs: IConfig;
-        plugins: IPlugins;
-        firebase: IFirebase;
-        browser: Browser;
-        prisma: PrismaClient;
-    }
+  interface FastifyInstance {
+    configs: IConfig;
+    plugins: IPlugins;
+    libs: ILibraries;
+  }
 
-    interface FastifyRequest {
-        user: IJWTPayload;
-    }
+  interface FastifyReply {
+    user: IJWTPayload;
+  }
 }
